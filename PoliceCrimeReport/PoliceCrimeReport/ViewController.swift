d9ebad9308026fd00a7205745dd8803f8d5e41c9
Rastislav Smolen <>
2021-01-06 03:46:10 +0000

    0000 Clean Build

---
+++ PoliceCrimeReport/PoliceCrimeReport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PoliceCrimeReport
+//
+//  Created by Rastislav Smolen on 06/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
